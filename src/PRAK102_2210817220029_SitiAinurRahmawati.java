7578489e14bee61f2e7d8febd8e80312053e6f5f
Siti Ainur Rahmawati <2210817220029@mhs.ulm.ac.id>
2023-09-16 07:18:58 +0800

    first commit

---
+++ src/PRAK102_2210817220029_SitiAinurRahmawati.java
@@ -0,0 +1,12 @@
+package PRAKTIKUM1.src;
+
+public class PRAK102_2210817220029_SitiAinurRahmawati {
+    public static void main(String[] args) {
+        System.out.println("############################");
+        System.out.println("#                          #");
+        System.out.println("#   Siti Ainur Rahmawati   #");
+        System.out.println("#       2210817220029      #");
+        System.out.println("#                          #");
+        System.out.println("############################");
+    }
+}
---
+++ src/PRAK104_2210817220029_SitiAinurRahmawati.java
@@ -0,0 +1,9 @@
+package PRAKTIKUM1.src;
+
+public class PRAK104_2210817220029_SitiAinurRahmawati {
+    public static void main(String[] args) {
+        String nama = "Siti Ainur Rahmawati";
+        int umur = 19;
+        System.out.printf("Perkenalkan, Nama Saya %s, Umur Saya %d", nama, umur);
+    }
+}
