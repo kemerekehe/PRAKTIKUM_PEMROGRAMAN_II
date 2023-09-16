1a12c4a273a7ff87fb92db8cd57a34939966faf7
Siti Ainur Rahmawati <2210817220029@mhs.ulm.ac.id>
2023-09-16 18:16:50 +0800

    first commit
    Signed-off-by: Siti Ainur Rahmawati <2210817220029@mhs.ulm.ac.id>

---
+++ MODUL1/PRAK101_2210817220029_SitiAinurRahmawati.java
@@ -0,0 +1,9 @@
+package MODUL1;
+
+public class PRAK101_2210817220029_SitiAinurRahmawati {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, Siti Ainur Rahmawati");
+        System.out.println("Selamat Siang, Siti Ainur Rahmawati");
+        System.out.println("Selamat Malam, Siti Ainur Rahmawati");
+    }
+}
