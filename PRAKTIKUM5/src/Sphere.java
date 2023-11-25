eb8a049d54e713bc64b419ea96c5110b7a3b2de1
Siti Ainur Rahmawati <2210817220029@mhs.ulm.ac.id>
2023-11-26 00:59:31 +0800

    pertama

---
+++ PRAKTIKUM5/src/Sphere.java
@@ -0,0 +1,15 @@
+public class Sphere extends Shape {
+    private double radius;
+
+    public Sphere (double r) {
+        super("Sphere");
+        radius = r;
+    }
+    @Override
+    public double area() {
+        return 4*Math.PI*(radius*radius);
+    }
+    public String toString() {
+        return super.toString() + " of radius " + radius;
+    }
+}
