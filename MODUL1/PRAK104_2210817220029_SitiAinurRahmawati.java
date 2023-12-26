0ef31b6d81125f664c21f3b4452f67a0a790174f
Siti Ainur Rahmawati <114901278+kemerekehe@users.noreply.github.com>
2023-12-26 10:39:15 +0800

    Delete MODUL1 directory

--- MODUL1/PRAK104_2210817220029_SitiAinurRahmawati.java
+++
@@ -1,9 +0,0 @@
-package MODUL1;
-
-public class PRAK104_2210817220029_SitiAinurRahmawati {
-    public static void main(String[] args) {
-        String nama = "Siti Ainur Rahmawati";
-        int umur = 19;
-        System.out.printf("Perkenalkan, Nama Saya %s, Umur Saya %d", nama, Integer.valueOf(umur));
-    }
-}
